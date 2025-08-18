680c80c49f693d17164eeec4976c25ccbc553bd6
prasad <prasad@mamenta.com>
2025-08-18 14:36:19 +0530

    test fixes

--- src/test.py
+++ src/test.py
@@ -10,6 +10,7 @@ import pandas
 import yaml
 
 import src.wbr as wbr
+from src import validator
 from src.controller_utility import SixTwelveChart, TrailingTable, get_wbr_deck, SafeLineLoader
 
 test_suite_folder = Path(os.path.dirname(__file__)) / 'unit_test_case'
@@ -100,9 +101,20 @@ def test_wbr():
 
         config = yaml.load(open(config_file_path), SafeLineLoader)
         test_config = yaml.safe_load(open(test_config_file))
+
+        try:
+            wbr_validator = validator.WBRValidator(
+                cfg=config,
+                csv_data=csv_file
+            )
+            wbr_validator.validate_yaml()
+        except Exception as e:
+            logging.error(f"WBR Validation or data loading failed: {e}", exc_info=True)
+            raise Exception(f"Invalid configuration or data loading error: {e}")
+
         try:
             # Create a WBR object using the CSV data and configuration
-            wbr1 = wbr.WBR(config, csv=csv_file)
+            wbr1 = wbr.WBR(config, daily_df=wbr_validator.daily_df)
         except Exception as error:
             logging.error(error, exc_info=True)
             raise error
