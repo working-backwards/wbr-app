cb84bf4a709d2f202472ae1d005ed3e55e34a19a
vedminds <info@vedminds.com>
2026-03-05 12:51:04 +0530

    reverted the SafeLoader constructor call

--- src/controller_utility.py
+++ src/controller_utility.py
@@ -95,7 +95,7 @@ class Encoder(JSONEncoder):
 
 class SafeLineLoader(SafeLoader):
     def construct_mapping(self, node, deep=False):
-        mapping = super().construct_mapping(node, deep=deep)
+        mapping = super(SafeLineLoader, self).construct_mapping(node, deep=deep)
         # Add 1 so line numbering starts at 1
         mapping["__line__"] = node.start_mark.line + 1
         return mapping
